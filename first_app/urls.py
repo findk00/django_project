4708db1759b4f9c5e17d2e4955db64456c8dd442
findk00 <findkfindk1@gmail.com>
2021-06-03 16:57:53 +0900

    코멘트

--- first_app/urls.py
+++ first_app/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-import first_app.views
+# from django.contrib import admin
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', first_app.views.home, name='home'),
-    path('first_app/', include('first_app.urls')),
+    path('new/', views.new, name='new'),
+    path('detail/<int:blog_id>/', views.detail, name='detail'),
 ]
