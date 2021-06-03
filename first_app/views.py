93c43c39dcf4e4158a5e9ec2fcf6fe456ef3e953
findk00 <findkfindk1@gmail.com>
2021-06-03 17:28:52 +0900

    코멘트

--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.utils import timezone
 from .models import Blog
+
 # Create your views here.
 
 def home(request):
@@ -9,6 +11,18 @@ def home(request):
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'detail.html', {'blog' : blog_detail})
+
+def create(request):
+    return render(request, 'create.html')
+
+def postcreate(request):
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    return redirect('/first_app/detail/' + str(blog.id))
+
 
 def new(request):
     full_text = request.GET['fulltext']
