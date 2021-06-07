aeadb89e5d74e5915d95ea63ba5e9e111ab15f12
findk00 <findkfindk1@gmail.com>
2021-06-07 18:00:28 +0900

    코멘트

--- first_project/urls.py
+++ first_project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 import first_app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', first_app.views.home, name='home'),
     path('first_app/', include('first_app.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
